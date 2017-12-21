86e3f5a081ebaf0dc0135c94aac10196039aa651
unknown <lynam.chris@gmail.com>
2017-12-20 20:48:30 -0600

    working app

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,20 +1,52 @@
 var friendArray = [
 	{
-	  "name":"Ahmed",
-	  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+	  "name":"Donny Boy",
+	  "photo":"https://cmgajcjaybookman.files.wordpress.com/2016/07/donald-trump.jpg",
 	  "scores":[
-	      5,
+	      1,
 	      1,
 	      4,
-	      4,
+	      1,
 	      5,
 	      1,
 	      2,
-	      5,
+	      1,
 	      4,
 	      1
 	    ]
-	}
+	},
+	{
+		"name":"Donald Duck",
+		"photo":"https://i.imgur.com/csfRVzW.jpg",
+		"scores":[
+			1,
+			2,
+			4,
+			1,
+			3,
+			1,
+			2,
+			4,
+			4,
+			5
+		  ]
+	  },
+	  {
+		"name":"Mr. T",
+		"photo":"https://vignette.wikia.nocookie.net/disney/images/5/5c/Mr-t.jpg/revision/latest?cb=20130803083139",
+		"scores":[
+			5,
+			2,
+			4,
+			5,
+			3,
+			5,
+			2,
+			4,
+			4,
+			1
+		  ]
+	  }
 
 ];
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,51 +1,95 @@
-// Dependencies
+// LOAD DATA
 var friendsData = require("../data/friends.js");
-var fs = require("fs");
-var path = require("path");
 
-
-// Routing
+// ROUTING
 module.exports = function(app) {
-
 
   app.get("/api/friends", function(req, res) {
     res.json(friendsData);
   });
 
+  // API POST Requests
+  // Below code handles when a user submits a form and thus submits data to the server.
+  // In each of the below cases, when a user submits form data (a JSON object)
+  // ...the JSON is pushed to the appropriate JavaScript array
+  // (ex. User fills out a reservation request... this data is then sent to the server...
+  // Then the server saves the data to the friendsData array)
+  // ---------------------------------------------------------------------------
 
   app.post("/api/friends", function(req, res) {
 
-    console.log(req.body);
+    console.log("1: " + req.body)
 
-    fs.readFile(path.join(__dirname, "../data/friends.json"), 'utf-8', function(err, data) {
-      if (err) throw err
+    res.json(calcDifference(req.body));
 
-      var friendsArray = JSON.parse(data);
-
-      console.log(friendsArray);
-
-      // for (var i = 0; i < friendsArray.length; i++) {
-      //   for (var j = 0; j < friendsArray[i].length; j++){
-      //     var difference = Math.abs(friendsArray[i].scores[])
-      //   }
-      // }
-
-      friendsArray.friends.push(req.body);
-
-      console.log(friendsArray);
-
-      fs.writeFile(path.join(__dirname, "../data/friends.json"), JSON.stringify(friendsArray), 'utf-8', function(err) {
-        if (err) throw err
-        console.log('Done!')
-      })
-    })
-
-
-      //friendsData.push(req.body);
-
-
-      res.json(req.body);
-      
   });
 
+};
+
+
+// Function to calculate score differences
+function calcDifference(userInput) {
+
+  console.log("CD");
+
+  var individualDifferenceArray = [];
+  var individualDifference;
+  var differenceArray = [];
+
+  for (var i = 0; i < friendsData.length; i++) {
+
+    console.log("Loop 1");
+    console.log("Friends Data: " + friendsData[i]);
+
+    individualDifferenceArray = [];
+
+    for (var j = 0; j < friendsData[i].scores.length; j++) {
+
+      console.log("Loop 2");
+
+      console.log("A: " + friendsData[i].scores[j]);
+
+      var difference = Math.abs(friendsData[i].scores[j] - userInput.scores[j]);
+      individualDifferenceArray.push(difference);
+      console.log("Inner: " + individualDifferenceArray);
+
+    }
+    individualDifference = individualDifferenceArray.reduce(getSum);
+    console.log("Outer: " + individualDifference);
+    differenceArray.push(individualDifference);
+    console.log("differenceArray: " + differenceArray);
+  }
+  var bestMatch = friendsData[minimunDifference(differenceArray)];
+  console.log("Best Match: " + bestMatch);
+  return bestMatch;
+}
+
+// Function to get sum of differences
+function getSum(total, num) {
+  return total + num;
+}
+
+// Function to get minimun difference
+function minimunDifference(differenceArray) {
+  console.log(differenceArray);
+  var minDifference = Math.min.apply(Math, differenceArray);
+  console.log("minDifference: " + minDifference);
+  for (k = 0; k < differenceArray.length; k++) {
+    if(differenceArray[k] === minDifference) {
+      console.log("4: " + k);
+      return k;
+    }
+  }
+}
+
+// Function to calculate independent differences
+function individualDifferences(i) {
+  for (var j = 0; j < friendsArray[i].length; j++) {
+
+    console.log("A: " + friendsArray[i].scores[j]);
+
+    var difference = Math.abs(friendsArray[i].scores[j] - userInput.scores[j]);
+    individualDifferenceArray.push(difference);
+    console.log("Inner: " + individualDifferenceArray);
+  }
 }
