d52b4b3ce2d75b12deb643b7e80fad402ace3d66
unknown <lynam.chris@gmail.com>
2017-12-21 18:26:13 -0600

    Deleted console logs

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,8 +10,6 @@ module.exports = function(app) {
 
   app.post("/api/friends", function(req, res) {
 
-    console.log("1: " + req.body)
-
     res.json(calcDifference(req.body));
 
     friendsData.push(req.body);
@@ -24,7 +22,6 @@ module.exports = function(app) {
 // Function to calculate score differences
 function calcDifference(userInput) {
 
-  console.log("CD");
 
   var individualDifferenceArray = [];
   var individualDifference;
@@ -32,29 +29,21 @@ function calcDifference(userInput) {
 
   for (var i = 0; i < friendsData.length; i++) {
 
-    console.log("Loop 1");
-    console.log("Friends Data: " + friendsData[i]);
 
     individualDifferenceArray = [];
 
     for (var j = 0; j < friendsData[i].scores.length; j++) {
 
-      console.log("Loop 2");
 
-      console.log("A: " + friendsData[i].scores[j]);
 
       var difference = Math.abs(friendsData[i].scores[j] - userInput.scores[j]);
       individualDifferenceArray.push(difference);
-      console.log("Inner: " + individualDifferenceArray);
 
     }
     individualDifference = individualDifferenceArray.reduce(getSum);
-    console.log("Outer: " + individualDifference);
     differenceArray.push(individualDifference);
-    console.log("differenceArray: " + differenceArray);
   }
   var bestMatch = friendsData[minimunDifference(differenceArray)];
-  console.log("Best Match: " + bestMatch);
   return bestMatch;
 }
 
@@ -65,12 +54,9 @@ function getSum(total, num) {
 
 // Function to get minimun difference
 function minimunDifference(differenceArray) {
-  console.log(differenceArray);
   var minDifference = Math.min.apply(Math, differenceArray);
-  console.log("minDifference: " + minDifference);
   for (k = 0; k < differenceArray.length; k++) {
     if(differenceArray[k] === minDifference) {
-      console.log("4: " + k);
       return k;
     }
   }
